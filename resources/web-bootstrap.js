287068dbd3698adb029a7f5688d50d856a2250f4
jeff-lewis <jlewis@streetconnect.com>
2015-03-23 23:03:16 -0400

    Added IIFE to angular.boot and jquery error msg logging

--- resources/web-bootstrap.js
+++ resources/web-bootstrap.js
@@ -1,30 +1,37 @@
-'use strict';
-/* global jQuery:true */
+(function (window, angular) {
+  'use strict';
+  /* global jQuery:true */
 
-angular.element(document).ready(function() {
-  //Fixing facebook bug with redirect
-  if (window.location.hash === '#_=_') window.location.hash = '#!';
+  angular.element(document).ready(function () {
+    //Fixing facebook bug with redirect
+    if (window.location.hash === '#_=_') window.location.hash = '#!';
 
-  //Then init the app
-  angular.bootstrap(document, ['mean']);
+    //Then init the app
+    angular.bootstrap(document, ['mean'], {strictDi: true});
 
-});
+  });
 
-function processModules(modules) {
-  var packageModules = ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router'],m,mn;
-  for (var index in modules) {
-    m = modules[index];
-    mn = 'mean.'+m.name;
-    angular.module(mn, m.angularDependencies || []);
-    packageModules.push(mn);
+  function processModules(modules) {
+    var packageModules = ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router'], m, mn;
+    for (var index in modules) {
+      m = modules[index];
+      mn = 'mean.' + m.name;
+      angular.module(mn, m.angularDependencies || []);
+      //console.log('loading module: ' + mn, m.angularDependencies, m);
+      packageModules.push(mn);
+    }
+
+    angular.module('mean', packageModules);
   }
 
-  angular.module('mean', packageModules);
-}
+  jQuery.ajax('/_getModules', {
+    dataType: 'json',
+    async: false,
+    success: processModules,
+    error: function (jqXHR, msg, err) {
+      console.error(msg, err, err.stack, jqXHR);
+    }
 
-jQuery.ajax('/_getModules', {
-  dataType: 'json',
-  async:false,
-  success: processModules
-});
+  });
 
+})(window, window.angular);
